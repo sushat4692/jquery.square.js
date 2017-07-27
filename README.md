b423675932521da20ab447afc7b8e511989f6189
sus-happy <sush@sus-happy.net>
2017-07-27 16:44:34 +0800

    v 0.1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+## jquery.square.js
+
+Simple adjusting layout library for Square Grid Layout
+
+### Changelog
+
+* Jul 27, 2017 v.0.1.0
+  * Support percent parameter
+  * Add destroy function
+* Jun 15, 2011 v.0.0.1
+  * First commit
+
+---
+
+Created By SUSH
+
--- js/jquery.square.js
+++ js/jquery.square.js
@@ -1,166 +1,334 @@
-( function($) {
-	$.fn.square = function( opt ) {
-		var _set = {
-			target : "square",
-			inner : ".inner",
-			scaleSplit : "_",
-			size : 150,
-			space : 10,
-			speed : 700,
-			ease : ""
-		}
-		if( opt ) $.extend( _set, opt );
-		
-		var _parent = $(this).css( {position:"relative"} );
-		var _target = _parent.find( "*[class^="+_set.target+"]" ).css({position:"absolute"});
-		var _resizeInter;
-		var _prevWidth = _parent.outerWidth();
-		setSize();
-		setPosition( 0 );
-		$(window).bind("resize",resizeStart);
+/**
+ * jquery.square.js
+ * Simple adjusting layout library for Square Grid Layout
+ * 
+ * @version 0.1.0
+ * @author SUSH <sush@sus-happy.net>
+ * @license MIT
+ */
+(function($) {
 
-		function resizeStart() {
-			$(window).unbind("resize")
-			_resizeInter = setInterval( resizeCheck, 500);
-		}
-		function resizeCheck() {
-			_prevWidth = _parent.outerWidth();
-			if( _prevWidth == _parent.outerWidth() ) {
-			clearInterval( _resizeInter );
-			$(window).bind("resize",resizeStart);
-				resizeEnd();
-			}
-		}
-		function resizeEnd() {
-			setPosition( _set.speed );
-		}
-		
-		function setSize() {
-			_target.each( function(i) {
-				var scale = getScale( $(this).attr("class") );
-				var xsize = _set.size*scale.x+_set.space*(scale.x-1);
-				var ysize = _set.size*scale.y+_set.space*(scale.y-1);
+    var Square = function($target, opt) {
+        var self = this;
 
-				$(this).css( { width:xsize, height:ysize } );
-				if( $(this).find(_set.inner).length > 0 ) {
-					var inner = $(this).find(_set.inner);
-					inner.height( ysize );
-					
-					if( inner.outerHeight() > ysize ) {
-						var height = inner.height()-( inner.outerHeight()-ysize );
-						inner.height( height );
-					}
-				}
-			} );
-		}
-		
-		function setPosition( sp ) {
-			var matrix = new Array();
-			var mpos = new Object();
-			var tHeight = 0;
-			_target.each( function(i) {
-				mpos = blankMatrix( matrix );
-				
-				var scale = getScale( $(this).attr("class") );
-				var sizex = _set.size*scale.x+_set.space*(scale.x-1);
-				var sizey = _set.size*scale.y+_set.space*(scale.y-1);
-				var flag = true;
-				
-				if( checkSize( mpos.x, scale ) ) {
-					mpos.x = 0;
-					mpos.y ++;
-				}
-				while( matrixCheck( matrix, mpos.x, mpos.y, scale ) ) {
-					mpos.x++;
-					if( checkSize( mpos.x, scale ) ) {
-						mpos.x = 0;
-						mpos.y ++;
-					}
-				}
-				
-				for( var x=0; x<scale.x; x++ ) {
-					for( var y=0; y<scale.y; y++ ) {
-						if( !matrix[mpos.x+x] ) matrix[mpos.x+x] = new Array();
-						matrix[mpos.x+x][mpos.y+y] = true;
-					}
-				}
-				
-				var pos = {
-					x:_set.space+_set.size*mpos.x+_set.space*mpos.x,
-					y:_set.space+_set.size*mpos.y+_set.space*mpos.y
-				};
-				if( sp > 0 ) {
-					goTween( $(this), { left:pos.x, top:pos.y }, sp );
-				} else {
-					$(this).css( { left:pos.x, top:pos.y } );
-				}
-				
-				tHeight = pos.y+sizey+_set.space > tHeight ? pos.y+sizey+_set.space : tHeight;
-			} );
-			if( sp > 0 ) {
-				goTween( _parent, { height:tHeight }, sp );
-			} else {
-				_parent.css( { height:tHeight } );
-			}
-		}
-		
-		function getScale( clsName ) {
-			var tmp = clsName.split(" ");
-			for( i in tmp ) {
-				if( tmp[i].match( _set.scaleSplit ) ) {
-					var xNum = tmp[i].split( _set.scaleSplit )[1];
-					var yNum = tmp[i].split( _set.scaleSplit )[2];
-					if( xNum ) {
-						if( yNum )
-							return {x:xNum, y:yNum};
-						else
-							return {x:xNum, y:xNum};
-					}
-				}
-			}
-			return { x:1, y:1 };
-		}
-		
-		function matrixCheck( matrix, x, y, scale ) {
-			if( !matrix ) {
-				return false;
-			}
-			for( var i=0; i<scale.x; i++ ) {
-				for( var j=0; j<scale.y; j++ ) {
-					if( !matrix[x+i] ) {
-						continue;
-					} else if( !matrix[x+i][y+j] ) {
-						continue;
-					}
-					return matrix[x+i][y+j];
- 				}
-			}
-			return false;
-		}
-		function blankMatrix( matrix ) {
-			var pos = {x:0, y:0};
-			while( matrixCheck( matrix, pos.x, pos.y, 1 ) ) {
-				pos.x++;
-				if( checkSize( pos.x, 1 ) ) {
-					pos.x = 0;
-					pos.y++;
-				}
-			}
-			return pos;
-		}
-		function checkSize( x, scale ) {
-			var size = _set.size*scale.x+_set.space*(scale.x-1);
-			var tsize = _set.size*x+_set.space*(x+2) + size;
-			return tsize >= _parent.outerWidth();
-		}
-		
-		function goTween( jObj, prop, sp ) {
-			if( _set.ease && $.easing[_set.ease] ) {
-				jObj.animate( prop, sp, _set.ease );
-			} else {
-				jObj.animate( prop, sp );
-			}
-		}
-		return $(this);
-	}
-} )(jQuery);
+        self.set = {
+            target: "square",
+            inner: ".inner",
+            scaleSplit: "_",
+            size: 150,
+            space: 10,
+            speed: 700,
+            duration: 500,
+            ease: ""
+        }
+        if(opt) $.extend(self.set, opt);
+
+        self.disabled    = false;
+
+        self.parentObj   = $target.css({position:"relative"});
+        self.target      = self.parentObj.find("*[class^="+self.set.target+"]").css({position:"absolute"});
+        self.resizeInter = null;
+        self.prevWidth   = self.parentObj.outerWidth();
+
+        self.size        = 0;
+        self.space       = 0;
+
+        // is size is floated?
+        self.isFloated   =
+            (isNaN(self.set.size) && self.set.size.match(/\%$/)) ||
+            (isNaN(self.set.space) && self.set.space.match(/\%$/));
+
+        self.setSize();
+        self.setPosition( 0 );
+        $(window).on("resize.square", function() {
+            self.resizeStart();
+        });
+    };
+
+    /**
+     * Starting resize window
+     */
+    Square.prototype.resizeStart = function() {
+        var self = this;
+
+        // If disabled, don't dunning
+        if(self.disabled) {
+            return;
+        }
+
+        $(window).off("resize.square")
+        self.resizeInter = setInterval( function() {
+            self.resizeCheck();
+        }, self.set.duration);
+    };
+
+    /**
+     * Checking resize window
+     */
+    Square.prototype.resizeCheck = function() {
+        var self = this;
+
+        // If disabled, don't dunning
+        if(self.disabled) {
+            return;
+        }
+
+        self.prevWidth = self.parentObj.outerWidth();
+
+        // if same width with previous width
+        if(self.prevWidth == self.parentObj.outerWidth()) {
+            clearInterval(self.resizeInter);
+            $(window).on("resize.square", function() {
+                self.resizeStart();
+            });
+            self.resizeEnd();
+        }
+    }
+
+    /**
+     * Ending resize window
+     */
+    Square.prototype.resizeEnd = function() {
+        var self = this;
+
+        // If disabled, don't dunning
+        if(self.disabled) {
+            return;
+        }
+
+        if(self.isFloated) {
+            self.setSize();
+        }
+        self.setPosition(self.set.speed);
+    }
+
+    /**
+     * Set each boxes size
+     */
+    Square.prototype.setSize = function() {
+        var self = this;
+        
+        // Calc box and space size
+        self.calcSize();
+
+        self.target.each( function(i) {
+            var scale = self.getScale($(this).attr("class"));
+            var xsize =
+                self.size*scale.x+
+                self.space*(scale.x-1);
+            var ysize =
+                self.size*scale.y+
+                self.space*(scale.y-1);
+
+            $(this).css( { width:xsize, height:ysize } );
+            if( $(this).find(self.set.inner).length > 0 ) {
+                var inner = $(this).find(self.set.inner);
+                inner.height( ysize );
+
+                if( inner.outerHeight() > ysize ) {
+                    var height = inner.height()-( inner.outerHeight()-ysize );
+                    inner.height( height );
+                }
+            }
+        } );
+    };
+
+    /**
+     * Set position each boxes
+     */
+    Square.prototype.setPosition = function(sp) {
+        var self = this;
+
+        var matrix  = [];
+        var mpos    = {};
+        var tHeight = 0;
+
+        self.target.each( function(i) {
+            mpos = self.blankMatrix( matrix );
+
+            var scale = self.getScale($(this).attr("class"));
+            var sizex =
+                self.size*scale.x+
+                self.space*(scale.x-1);
+            var sizey =
+                self.size*scale.y+
+                self.space*(scale.y-1);
+            var flag  = true;
+
+            if(self.checkSize(mpos.x, scale)) {
+                mpos.x = 0;
+                mpos.y ++;
+            }
+            while(self.matrixCheck(matrix, mpos.x, mpos.y, scale)) {
+                mpos.x++;
+                if(self.checkSize(mpos.x, scale)) {
+                    mpos.x = 0;
+                    mpos.y ++;
+                }
+            }
+
+            for(var x=0; x<scale.x; x++) {
+                for(var y=0; y<scale.y; y++) {
+                    if(! matrix[mpos.x+x]) matrix[mpos.x+x] = [];
+                    matrix[mpos.x+x][mpos.y+y] = true;
+                }
+            }
+
+            var pos = {
+                x:self.space+self.size*mpos.x+
+                  self.space*mpos.x,
+                y:self.space+self.size*mpos.y+
+                  self.space*mpos.y
+            };
+            if( sp > 0 ) {
+                self.goTween($(this), {left:pos.x, top:pos.y}, sp);
+            } else {
+                $(this).css({left:pos.x, top:pos.y});
+            }
+
+            tHeight = pos.y+sizey+self.space > tHeight ?
+                      pos.y+sizey+self.space :
+                      tHeight;
+        } );
+        if( sp > 0 ) {
+            self.goTween(self.parentObj, {height:tHeight}, sp);
+        } else {
+            self.parentObj.css({height:tHeight});
+        }
+    };
+
+    /**
+     * Get box scale
+     */
+    Square.prototype.getScale = function(clsName) {
+        var self = this;
+
+        var tmp = clsName.split(" ");
+        for(i in tmp) {
+            if( tmp[i].match(self.set.scaleSplit)) {
+                var xNum = tmp[i].split(self.set.scaleSplit)[1];
+                var yNum = tmp[i].split(self.set.scaleSplit)[2];
+                if( xNum ) {
+                    if( yNum )
+                        return {x:xNum, y:yNum};
+                    else
+                        return {x:xNum, y:xNum};
+                }
+            }
+        }
+        return {x:1, y:1};
+    };
+
+    /**
+     * Check avaivable matrix position
+     */
+    Square.prototype.matrixCheck = function(matrix, x, y, scale) {
+        var self = this;
+    
+        if(! matrix) {
+            return false;
+        }
+        for(var i=0; i<scale.x; i++) {
+            for( var j=0; j<scale.y; j++ ) {
+                if(! matrix[x+i]) {
+                    continue;
+                } else if(! matrix[x+i][y+j]) {
+                    continue;
+                }
+                return matrix[x+i][y+j];
+            }
+        }
+        return false;
+    };
+
+    /**
+     * Search blank position of matrix
+     */
+    Square.prototype.blankMatrix = function(matrix) {
+        var self = this;
+
+        var pos = {x:0, y:0};
+        while(self.matrixCheck(matrix, pos.x, pos.y, 1)) {
+            pos.x++;
+            if(self.checkSize(pos.x, 1)) {
+                pos.x = 0;
+                pos.y++;
+            }
+        }
+        return pos;
+    };
+
+    /**
+     * Check size
+     */
+    Square.prototype.checkSize = function(x, scale) {
+        var self = this;
+
+        var size  =
+            self.size*scale.x+
+            self.space*(scale.x-1);
+        var tsize =
+            self.size*x+
+            self.space*(x+2) + size;
+        return tsize > self.parentObj.outerWidth();
+    };
+
+    /**
+     * Running animation of target object
+     */
+    Square.prototype.goTween = function( jObj, prop, sp ) {
+        var self = this;
+
+        if(self.set.ease && $.easing[self.set.ease]) {
+            jObj.stop(true, false).animate(prop, sp, self.set.ease);
+        } else {
+            jObj.stop(true, false).animate(prop, sp);
+        }
+    };
+
+    /**
+     * Calculate size
+     */
+    Square.prototype.calcSize = function() {
+        var self = this;
+
+        // Space
+        if(! isNaN(self.set.space)) {
+            self.space = self.set.space;
+        } else if(self.set.space.match(/\%$/)) {
+            var per = (self.set.space.replace('%', '')) -0;
+            self.space = self.parentObj.width()*(per/100);
+        }
+
+        // Size
+        if(! isNaN(self.set.size)) {
+            self.size = self.set.size;
+        } else if(self.set.size.match(/\%$/)) {
+            var per = (self.set.size.replace('%', '')) -0;
+            var spaces = Math.ceil(100/per);
+
+            self.size =
+                (self.parentObj.width() -(self.space*(spaces+1)))*
+                (per/100);
+        }
+    };
+
+    /**
+     * Destroy
+     */
+    Square.prototype.destroy = function() {
+        var self = this;
+        self.disabled = true;
+
+        if(self.resizeInter) {
+            clearInterval(self.resizeInter);
+        }
+        $(window).off(".square");
+        self.parentObj.attr('style', '');
+        self.target.attr('style', '');
+    };
+
+    $.fn.square = function(opt) {
+        return new Square($(this), opt);
+    };
+})(jQuery);
